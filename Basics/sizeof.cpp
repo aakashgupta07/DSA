0c1aa8c79588c92fca97bfd1ff10b27e32d26989
Aakash Gupta <aakashhprof@gmail.com>
2023-08-01 23:24:31 +0530

    size of  datatype

---
+++ Basics/sizeof.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    cout << "size of integer = " << sizeof(a) << endl;
+
+    short b = 5;
+    cout << "size of short = " << sizeof(b) << endl;
+
+    char c = 5;
+    cout << "size of character = " << sizeof(c) << endl; 
+
+    bool d = 5;
+    cout << "size of boolean = " << sizeof(d) << endl;
+
+    float e = 5;
+    cout << "size of float = " << sizeof(e) << endl;
+
+    double f = 5;
+    cout << "size of double = " << sizeof(f) << endl;
+
+    long g = 5;
+    cout << "size of long = " << sizeof(g) << endl;
+
+    long long h = 5;
+    cout << "size of long-long = " << sizeof(h) << endl;
+
+
+}
