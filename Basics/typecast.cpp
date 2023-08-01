2c7368be19c4ee4da02413aed093d35bedd48ae0
Aakash Gupta <aakashhprof@gmail.com>
2023-08-01 23:42:04 +0530

    type casting

---
+++ Basics/typecast.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch = 97;
+    cout << ch << endl;
+
+    int i = 'b';
+    cout << i << endl;
+
+    double d = 9.5;
+    int x = (int)d + 2;
+    cout << "value of x is " << x << endl;
+}
