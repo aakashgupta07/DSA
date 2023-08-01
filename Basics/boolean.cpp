e0fbcfd1ca16c89e774bc49a0432e073ead7afd8
Aakash Gupta <aakashhprof@gmail.com>
2023-08-01 23:02:24 +0530

    boolean values

---
+++ Basics/boolean.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool y = true;
+    cout << "value of y is " << y << endl;
+
+    bool z = false;
+    cout << "value of y is " << z << endl;
+}
