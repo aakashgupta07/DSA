243ea9addb0dd95c0775a44ab4536ea2a7c563e0
Aakash Gupta <aakashhprof@gmail.com>
2023-08-03 00:57:06 +0530

    inverted half pyramid

---
+++ patterns/inv-half-pyr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter a size of pyramid" << endl;
+    int rowCount;
+    cin >> rowCount;
+    
+    for (int row=0; row<rowCount; row++)
+    {
+        for (int col=0; col<rowCount-row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
