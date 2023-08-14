14ed81f128d5db1b2598658c62745414f2b384bb
Aakash Gupta <aakashhprof@gmail.com>
2023-08-15 00:24:12 +0530

    add function

--- bubble-sort.c
+++ bubble-sort.c
@@ -13,4 +13,37 @@ void main()
     printf("array a[%d] = ",i);
     scanf("%d",&a[i]);
   }
+  bubble_sort(a,n);
+  display(a,n);
+}
+
+void bubble_sort(int a[MAX], int n)
+{
+  int i,j,k,temp;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n-i-1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+        temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;
+      }
+      printf("\n");
+      for(k=0;k<n;k++)
+      {
+        printf("\tarray[%d]=%d",k,a[k]);
+      }
+    }
+  }
+}
+
+void display(int a[MAX], int n)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+  printf("\na[%d]=%d",i,a[i]);
+  }
 }
