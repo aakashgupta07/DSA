51d39958aad2faa7e13117a8b818ed0fec74280c
Aakash Gupta <aakashhprof@gmail.com>
2023-08-14 23:48:39 +0530

    add function

--- binary-search.c
+++ binary-search.c
@@ -10,4 +10,31 @@ void main()
   }
   printf("\nEnter a search value");
   scanf("%d",&val);
-}
+  j=bsearch(a,10,val);
+  if(j==0)
+    printf("searched value found \n");
+    else 
+    printf("searched value not found");
+  }
+  int bsearch(int a[],int n, int x)
+  {
+    int low=0;
+    int high=n-1;
+    int flag=1;
+    int mid;
+    while(low<=high)
+    {
+      mid=(high+low)/2;
+      if(x<a[mid])
+      {
+        high=mid-1;
+      } else if(x>a[mid])
+      {
+        low=mid+1;
+      } else if(x==a[mid])
+      {
+        return 0;
+      }
+    }
+      return flag;
+  }
